0941c7f6468f6a64d9637ff95e6571cd9b7c6ade
Antonio Calapez <antonio.root@gmail.com>
2016-11-29 11:40:00 +0100

    We can now save state in localStorage

---
+++ web-client/src/models/localStorage.js
@@ -0,0 +1,20 @@
+export const loadState = () => {
+	try {
+		const serializedState = localStorage.getItem('state')
+		if(serializedState === null){
+			return undefined
+		}
+		return JSON.parse(serializedState)
+	} catch (err){
+		return undefined
+	}
+}
+
+export const saveState = (state) => {
+	try {
+		const serializedStae = JSON.stringify(state)
+		localStorage.setItem('state', serializedStae)
+	} catch (err){
+		
+	}
+}

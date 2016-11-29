7a9d01c340b7c99499042104a123c7d7015765f1
anymaniax <anymaniax@Anymaniax.local>
2016-11-29 11:32:29 +0100

    Fix products

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -3,20 +3,20 @@ require('../models/db')
 let Product = require('../models/product')
 
 const PORT = process.env.PORT || 5000
-	// TODO:
-	// -> Apply point 7 from 
-	// http://blog.mwaysolutions.com/2014/06/05/10-best-practices-for-better-restful-api/
-	// -> Handler errors
+// TODO:
+// -> Apply point 7 from 
+// http://blog.mwaysolutions.com/2014/06/05/10-best-practices-for-better-restful-api/
+// -> Handler errors
 module.exports.getAll = (req, res) => {
 	Product.find((err, products) => {
-		if (!products) {
+		if(!products){
 			res.status(404)
 			res.json({
 				err: "No products found :("
 			})
 		}
 
-		if (err) {
+		if(err){
 			res.status(500)
 			return res.json({
 				err: "An unexpect error happened"
@@ -28,11 +28,11 @@ module.exports.getAll = (req, res) => {
 
 module.exports.getById = (req, res) => {
 	Product.findById(req.params.id, (err, product) => {
-		if (product) {
+		if(product){
 			return res.json(product)
 		}
-
-		if (err) {
+		
+		if(err){
 			res.status(404)
 			return res.json({
 				error: "No product found for that id."
@@ -44,7 +44,7 @@ module.exports.getById = (req, res) => {
 module.exports.addProduct = (req, res) => {
 	let product = Product(req.body)
 	product.save((err, product) => {
-		if (err) {
+		if(err){
 			res.status(406)
 			console.log(err)
 			return res.json({
@@ -61,10 +61,8 @@ module.exports.addProduct = (req, res) => {
 }
 
 module.exports.delProduct = (req, res) => {
-	Product.findOneAndRemove({
-		'_id': req.params.id
-	}, (err, product) => {
-		if (err) {
+	Product.findOneAndRemove({'_id': req.params.id}, (err, product) => {
+		if(err){
 			res.status(500)
 			return res.json('Could not remove this product :(')
 		}
@@ -75,12 +73,8 @@ module.exports.delProduct = (req, res) => {
 }
 
 module.exports.updateProduct = (req, res) => {
-	Product.update({
-		_id: req.params.id
-	}, {
-		$set: req.body
-	}, (err, product) => {
-		if (err) {
+	Product.update({ _id: req.params.id}, { $set: req.body }, (err, product) => {
+		if(err){
 			res.status(500)
 			return res.json('Could not update this product :(')
 		}

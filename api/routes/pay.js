cdfba3cad6e9f2ebd99a471fc0f4ba6d16f4ff4f
anymaniax <victor-bury@hotmail.com>
2017-01-23 22:57:09 +0100

    pay

--- api/routes/pay.js
+++ api/routes/pay.js
@@ -2,7 +2,12 @@ let router = require('express').Router()
 let ctrl = require('../controllers/pay')
 let auth = require('../controllers/auth')
 
-router.post('/:id?', auth.checkToken, ctrl.pay)
-router.get('/:id', ctrl.detail)
+const authId = [auth.checkToken, auth.requireId]
+const authMixin = [auth.checkToken, auth.requireAdmin]
+
+router.post('/:id', authId, ctrl.pay)
+router.get('/valid/:id', ctrl.valid)
+router.get('/payement/:id', authId,ctrl.getByIdPayement)
+router.get('/allPayement/', authMixin, ctrl.getAllPayement)
 
 module.exports = router
